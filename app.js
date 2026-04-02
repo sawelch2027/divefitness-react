c20957b1a48e2e6972a7d6b85d7e6446b2f502c4
Sawelch2027 <Sawelch@email.sc.edu>
2026-04-02 15:19:51 -0400

    project 10 almost complete

--- app.js
+++ app.js
@@ -45,4 +45,6 @@ app.post("/api/messages", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => 
+  console.log(`Server running on port ${PORT}`
+  ));
